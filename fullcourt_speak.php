8445f8a13a4e90e0c82a3462c74d6dde147a776e
tkosumi <tkosumi@gmail.com>
2012-10-26 08:35:17 +0900

    new file:   fullcourt_speak.php 	deleted:    fullcourt_availavlephonenumbers.php

---
+++ fullcourt_speak.php
@@ -0,0 +1,18 @@
+<?php
+	require_once 'fullcourt-php/fullcourt.php';
+	$attribute = array('language' => "ja",'voice' => "man");
+	$text = "これはフルコートのPHPスクリプトのテストです";
+
+	$response = new Response();
+
+	// Add Speak Element
+	$speak = $response->addSpeak($text,$attribute);
+
+	echo($response->toXML());
+
+	// Output:
+	// <Response>
+	// <Speak language="ja" voice="man">これはフルコートのPHPスクリプトのテストです</Speak>
+	// </Response>
+	// 
+?>
